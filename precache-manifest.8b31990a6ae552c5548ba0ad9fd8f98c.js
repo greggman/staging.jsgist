9894cc16ca530536aeb637dad3315f2bbf8e0445
greggman <github@greggman.com>
2021-12-13 04:50:53 +0000

    Deploying to gh-pages from  @ adc51a63c7e07d4e0d34cca762a754e2e3ae1ad1 🚀

---
+++ precache-manifest.8b31990a6ae552c5548ba0ad9fd8f98c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650a51c5ded2370081342ed1256b87d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59f91491bc0959dff12b",
+    "url": "/static/css/2.64939d6e.chunk.css"
+  },
+  {
+    "revision": "081481123500fb209c29",
+    "url": "/static/css/main.84f4815d.chunk.css"
+  },
+  {
+    "revision": "59f91491bc0959dff12b",
+    "url": "/static/js/2.17d4849a.chunk.js"
+  },
+  {
+    "revision": "39163606f80861140b373411a4b74b7d",
+    "url": "/static/js/2.17d4849a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081481123500fb209c29",
+    "url": "/static/js/main.b892e844.chunk.js"
+  },
+  {
+    "revision": "21d734a13edf31307f64",
+    "url": "/static/js/runtime-main.a136100e.js"
+  },
+  {
+    "revision": "e3f3298bd723b6f30e1417c8d69c8d49",
+    "url": "/static/media/horizontal-layout.e3f3298b.svg"
+  },
+  {
+    "revision": "8a8b9bc80c0f6dfe948b6c32fda5c841",
+    "url": "/static/media/tabbed-layout.8a8b9bc8.svg"
+  },
+  {
+    "revision": "23605ce8211e49b93768a9eb50e7ab30",
+    "url": "/static/media/two-by-two-layout.23605ce8.svg"
+  },
+  {
+    "revision": "f248a432a8be3e5ec10f7c2879fa2339",
+    "url": "/static/media/vertical-layout.f248a432.svg"
+  }
+]);
